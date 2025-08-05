2852b706a5bbc86791e489d1151c6d59f4f1cb7a
Marco Impellizzeri <impellizzeri.marco18@gmail.com>
2025-08-05 14:47:35 +0200

    aggiunto componente Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return(
+    <div className="flex justify-between items-center mx-10 my-6">
+      <h1 className="text-3xl">Header</h1>
+      <a href="https://github.com/MarcoImpellizzeri/counter" target="blank" >GitHub</a>
+    </div>
+  )
+}
